37d56302b680670347df0e8e630a63d0670b81f5
Dev-Harisrana <2018-pharmd-054@uvas.edu.pk>
2023-03-08 14:02:14 +0500

    //

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ function counter(n,t,id){
         window.addEventListener('onload',counter(50,30,number3));
         window.addEventListener('onload',counter(50,30,number4));
   
+
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "harisrana569@gmail.com",
+            Password : "34F8DFD0BD4C735DF114B18D1B9A1AA038F9",
+            To : 'harisrana569@gmail.com',
+            From : "you@isp.com",
+            Subject : "This is the subject",
+            Body : "And this is the body"
+        }).then(
+          message="thanks for contacting we will reach you soon",
+          message => alert(message)
+        );
